a415f76ddf07b8f4a290eb75f4fa05e39776cace
emanmo5050-oss <emanmo5050@gmail.com>
2026-03-09 23:31:17 +0200

    Add files via upload

---
+++ include/core.h
@@ -0,0 +1,6 @@
+#ifndef CORE_H
+#define CORE_H
+ // Here we will define all the functions you wrote in the core.cpp 
+ //so that other tems can use them without referring to the long code.
+
+#endif
